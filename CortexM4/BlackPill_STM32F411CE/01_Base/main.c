b4058831abdc2b5801334d5a601a40cf094f59e1
fatihkaptan <kaptan587@gmail.com>
2024-09-15 10:08:31 +0300

    base project

---
+++ CortexM4/BlackPill_STM32F411CE/01_Base/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+#include "main.h"
+
+//islemciler main'den baslamaz. reset vectorden baslar. 
+//startup kodda belirtilir. 
+
+void init(){
+
+}
+
+int main()
+{
+  init();
+  
+  while(1){
+     /* Insert 50 ms delay */
+   //Delay(50);
+    
+  }
+  
+  
+  //return 0;
+}
---
+++ CortexM4/BlackPill_STM32F411CE/01_Base/main.h
@@ -0,0 +1,6 @@
+#include "stm32f4xx.h"
+
+
+/* Exported functions ------------------------------------------------------- */
+void TimingDelay_Decrement(void);
+
